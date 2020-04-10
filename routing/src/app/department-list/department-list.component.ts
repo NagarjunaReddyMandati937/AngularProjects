7662a8e91aeaced21aac1512401c7ff6e60f02fd
nagarjunareddymandati937 <nagarjunamandati937@gmail.com>
2020-04-10 20:20:53 +0530

    Route parameters like id based results

--- routing/src/app/department-list/department-list.component.ts
+++ routing/src/app/department-list/department-list.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-department-list',
   templateUrl: './department-list.component.html',
   styleUrls: ['./department-list.component.css']
 })
 export class DepartmentListComponent implements OnInit {
- public departments = [
-    {"id":1,"name":"Angular"},
-    {"id":2,"name":"Node.Js"},
-    {"id":3,"name":"Spring"},
-    {"id":4,"name":"Collections"},
-    {"id":5,"name":"Bootstrap"},
-    ]
-  constructor() { }
+  public departments = [
+    { "id": 1, "name": "Angular" },
+    { "id": 2, "name": "Node.Js" },
+    { "id": 3, "name": "Spring" },
+    { "id": 4, "name": "Collections" },
+    { "id": 5, "name": "Bootstrap" },
+  ]
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  onSelect(department) {
+    this.router.navigate(['/departments', department.id]);
   }
 
 }
