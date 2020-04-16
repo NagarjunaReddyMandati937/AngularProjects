4a907c4f037065ded1f97e89de4581e41a8c10c0
nagarjunareddymandati937 <nagarjunamandati937@gmail.com>
2020-04-16 15:25:56 +0530

    modified content

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app1';
+  title = 'Angular Application 1';
+  name = 'Nagarjuna Reddy'
 }
