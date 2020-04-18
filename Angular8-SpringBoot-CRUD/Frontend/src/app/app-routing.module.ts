b24dfb9fe8e6c7a611f402c4c1da52f5db6828a5
nagarjunareddymandati937 <nagarjunamandati937@gmail.com>
2020-04-18 20:39:36 +0530

    Side Bar with more components added

--- Angular8-SpringBoot-CRUD/Frontend/src/app/app-routing.module.ts
+++ Angular8-SpringBoot-CRUD/Frontend/src/app/app-routing.module.ts
@@ -4,13 +4,33 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CareersComponent } from './careers/careers.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { AwardsComponent } from './awards/awards.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { SolutionsComponent } from './solutions/solutions.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'employees', component: EmployeeListComponent },
   { path: 'add', component: CreateEmployeeComponent },
   { path: 'update/:id', component: UpdateEmployeeComponent },
   { path: 'details/:id', component: EmployeeDetailsComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'careers', component: CareersComponent },
+  { path: 'technologies', component: TechnologiesComponent },
+  { path: 'awards', component: AwardsComponent },
+  { path: 'contactus', component: ContactusComponent },
+  { path: 'solutions', component: SolutionsComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- Angular8-SpringBoot-CRUD/Frontend/src/app/app.module.ts
+++ Angular8-SpringBoot-CRUD/Frontend/src/app/app.module.ts
@@ -8,13 +8,33 @@ import { EmployeeDetailsComponent } from './employee-details/employee-details.co
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { CareersComponent } from './careers/careers.component';
+import { AwardsComponent } from './awards/awards.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { SolutionsComponent } from './solutions/solutions.component';
 @NgModule({
   declarations: [
     AppComponent,
     CreateEmployeeComponent,
     EmployeeDetailsComponent,
     EmployeeListComponent,
-    UpdateEmployeeComponent
+    UpdateEmployeeComponent,
+    SignupComponent,
+    LoginComponent,
+    AboutusComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    CareersComponent,
+    AwardsComponent,
+    TechnologiesComponent,
+    ContactusComponent,
+    SolutionsComponent
   ],
   imports: [
     BrowserModule,
