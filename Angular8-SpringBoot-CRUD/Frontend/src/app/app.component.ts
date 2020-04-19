c063949314297bf9b988a19d90b1237f1eec654a
nagarjunareddymandati937 <nagarjunamandati937@gmail.com>
2020-04-19 12:04:54 +0530

    componets data added with CSS

--- Angular8-SpringBoot-CRUD/Frontend/src/app/app.component.ts
+++ Angular8-SpringBoot-CRUD/Frontend/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular 8 + Spring Boot + MySql';
+  title = 'Full Stack Web Application Using Rest APIs';
   name = 'N.R Technologis'
 }
