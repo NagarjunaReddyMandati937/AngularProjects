2ffaa23c8fa420432988a6fa2cc0136342f01ae1
nagarjunareddymandati937 <nagarjunamandati937@gmail.com>
2020-04-21 09:59:53 +0530

    validations for login and some css added

--- Angular8-SpringBoot-CRUD/Frontend/src/app/login/login.component.ts
+++ Angular8-SpringBoot-CRUD/Frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(3)]],
+        password: ['', Validators.required],
+      })
   }
-
 }
